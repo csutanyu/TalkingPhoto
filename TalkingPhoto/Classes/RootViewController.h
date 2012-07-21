627bc935d6b7a1a14617f8b4dfc1966fe759d069
Lancy <csutanyu@126.com>
2012-07-21 17:49:06 +0800

    test
    test

--- TalkingPhoto/Classes/RootViewController.h
+++ TalkingPhoto/Classes/RootViewController.h
@@ -13,7 +13,6 @@
 @interface RootViewController : UIViewController <JSAnimatedImagesViewDelegate, LeveyPopListViewDelegate>
 @property (retain, nonatomic) IBOutlet JSAnimatedImagesView *animatedImagesView;
 - (IBAction)takephoto:(id)sender;
-
 - (IBAction)photoLibrary:(id)sender;
 
 @end
