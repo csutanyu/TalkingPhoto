d985c638ba23e03816956d7f553904dcd42fef2b
csutanyu <csutanyu@126.com>
2012-07-21 17:39:24 +0800

    真正完全删除了build目录
    真正完全删除了build目录

--- build/TalkingPhoto.build/Debug-iphoneos/TalkingPhoto.build/Objects-normal/armv6/Photo2AudioFileEntity.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/tanyu/DusanApps/git_root/TalkingPhoto/TalkingPhoto/Classes/Photo2AudioFileEntity.m \
-  /Users/tanyu/DusanApps/git_root/TalkingPhoto/TalkingPhoto/Classes/Photo2AudioFileEntity.h
